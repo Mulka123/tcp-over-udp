a6dffd29038cc9d13f064bf46b3c0afce4ba3917
maeddz <maede.d.z@gmail.com>
2019-04-25 16:52:33 +0430

    handshake serverside

--- codes/TCPServerSocketImpl.java
+++ codes/TCPServerSocketImpl.java
@@ -3,6 +3,7 @@ import java.net.DatagramPacket;
 public class TCPServerSocketImpl extends TCPServerSocket {
     EnhancedDatagramSocket udp_socket;
     int port;
+    int state = 0;
     public TCPServerSocketImpl(int port) throws Exception {
         super(port);
         this.port = port;
@@ -12,11 +13,26 @@ public class TCPServerSocketImpl extends TCPServerSocket {
     public TCPSocket accept() throws Exception {
         udp_socket = new EnhancedDatagramSocket(port);
         byte[] b = new byte[256];
-        DatagramPacket dp = new DatagramPacket(b, b.length);
-        udp_socket.receive(dp);
+        DatagramPacket syn = new DatagramPacket(b, b.length);
+        udp_socket.receive(syn);
+        if(!(new String(syn.getData()).equals("SYN"))){
+            System.println("as");
+            return;
+        }
+        state = 1;
+        b = "SYN-ACK";
+        InetAddress address = InetAddress.getByName("127.0.0.1");
+        DatagramPacket ack = new DatagramPacket(b, b.length, address, syn.getPort());
+        udp_socket.send(ack);
+        state = 2;
+        DatagramPacket ack-syn = new DatagramPacket(b, b.length);
+        udp_socket.receive(ack-syn);
+        if(!(new String(ack-syn.getData()).equals("ACK"))){
+            System.println("aaa");
+            return;
+        }
+        state = 3;
         udp_socket.close();
-
-        System.out.println("HEY" + new String(b));
         return null;
     }
 
