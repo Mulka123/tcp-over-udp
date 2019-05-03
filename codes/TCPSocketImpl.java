50f918050d29abcd2eb4e55f171e3cff8d4ec28d
Mohammad Ali Toufighi <alitoumob@gmail.com>
2019-05-03 14:28:58 +0430

    implement receive and improve connect

--- codes/TCPSocketImpl.java
+++ codes/TCPSocketImpl.java
@@ -1,20 +1,27 @@
-import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.SocketTimeoutException;
+import java.util.*;
 
 public class TCPSocketImpl extends TCPSocket {
     private static final int MAX_PAYLOAD_SIZE = EnhancedDatagramSocket.DEFAULT_PAYLOAD_LIMIT_IN_BYTES;
     private EnhancedDatagramSocket udp_socket;
     private TCPState state;
-    private int next_sequence_number;
-    private int next_ack_number;
+    private int my_next_sequence_number;
+    private int my_last_ack_number;
+    private int expected_sequence_number;
+    private InetAddress address;
 
     public TCPSocketImpl(String ip, int port) throws Exception {
         super(ip, port);
-        this.port = port;
+        address = InetAddress.getByName(ip);
+        this.peer_port = port;
         this.state = TCPState.CLOSED; //TODO: check
     }
+
+    void setSeqNum(int seqNum) { my_next_sequence_number = seqNum; }
+
+    void setAckNum(int ackNum) { my_last_ack_number = ackNum; }
 
     void setUdpSocket(EnhancedDatagramSocket sock) {
         this.udp_socket = sock;
@@ -25,20 +32,27 @@ public class TCPSocketImpl extends TCPSocket {
         //TODO: check for state! don't connect if already connected
         udp_socket = new EnhancedDatagramSocket(8000); //TODO: how to choose port?
         udp_socket.setSoTimeout(5000); //TODO: how to set timeout?
-        InetAddress address = InetAddress.getByName("127.0.0.1");
 
+        my_next_sequence_number = Utils.randomInRange(50, 200); //havijoori :\
+
+        int synack_seq_num = 0;
+        int synack_ack_num;
         // send ACK and expect SYN-ACK
         TCPPacket packet = new TCPPacket(true, false);
-        byte[] buf = packet.toStream();
+        packet.setSeqNumber(my_next_sequence_number++);
         boolean syn_ack_received = false;
-        DatagramPacket dp = new DatagramPacket(buf, buf.length, address, port);
         while(!syn_ack_received) {
-            udp_socket.send(dp);
+            sendPacket(packet);
 
             state = TCPState.SYN_SENT;
-
             try {
                 TCPPacket recv_pkt = receivePacket();
+                synack_seq_num = recv_pkt.getSeqNum();
+                synack_ack_num = recv_pkt.getAckNumber();
+                if(synack_ack_num != my_next_sequence_number) {
+                    System.out.println("BAD PACKET :|");
+                    //TODO: what now?
+                }
                 if(recv_pkt.isSYN_ACK())
                     syn_ack_received = true;
             } catch (SocketTimeoutException e) {
@@ -50,12 +64,13 @@ public class TCPSocketImpl extends TCPSocket {
 
         // send ACK
         packet = new TCPPacket(false, true);
-        buf = packet.toStream();
-        dp = new DatagramPacket(buf, buf.length, address, port);
+        packet.setSeqNumber(my_next_sequence_number++);
+        packet.setAckNumber(++synack_seq_num);
         while(true){ //TODO: condition?
-            udp_socket.send(dp);
+            sendPacket(packet);
+
             try {
-                TCPPacket recv_pkt = this.receivePacket();
+                TCPPacket recv_pkt = receivePacket();
                 if(recv_pkt.isSYN_ACK())
                     continue;
             } catch (SocketTimeoutException e) {
@@ -64,7 +79,6 @@ public class TCPSocketImpl extends TCPSocket {
         }
 
         System.out.println("Client Established.");
-        this.next_sequence_number = Utils.randomInRange(50, 200); //havijoori :\
         state = TCPState.ESTABLISHED;
     }
 
@@ -75,6 +89,12 @@ public class TCPSocketImpl extends TCPSocket {
         return TCPPacket.toTCPPacket(recv_dp.getData());
     }
 
+    private void sendPacket(TCPPacket packet) throws Exception {
+        byte[] buf = packet.toStream();
+        DatagramPacket dp = new DatagramPacket(buf, buf.length, address, peer_port);
+        udp_socket.send(dp);
+    }
+
     @Override
     public void send(String pathToFile) throws Exception {
         throw new RuntimeException("Not implemented!");
@@ -82,7 +102,26 @@ public class TCPSocketImpl extends TCPSocket {
 
     @Override
     public void receive(String pathToFile) throws Exception {
-        byte[] data = "salam".getBytes();
+        ArrayList<TCPPacket> packets = new ArrayList<>();
+
+        while (true) {
+
+            TCPPacket recv_pkt = receivePacket();
+            packets.add(recv_pkt);
+
+            if(recv_pkt.getSeqNum() == expected_sequence_number) {
+                expected_sequence_number = recv_pkt.getSeqNum() + 1;
+                my_last_ack_number = recv_pkt.getSeqNum();
+            }
+
+            // send ACK
+            sendPacket(new TCPPacket(my_last_ack_number));
+
+            if(recv_pkt.isLastPacket())
+                break;
+        }
+        packets.sort(Comparator.comparing(TCPPacket::getSeqNum));
+        TCPPacket.saveToFile(packets, pathToFile);
     }
 
     @Override
