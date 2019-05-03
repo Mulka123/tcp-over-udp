42755caa32163af3045c869f1397899704713d94
Mohammad Ali Toufighi <alitoumob@gmail.com>
2019-05-03 14:40:34 +0430

    uncomment connect

--- codes/Sender.java
+++ codes/Sender.java
@@ -4,7 +4,7 @@ import java.net.*;
 public class Sender {
     public static void main(String[] args) throws Exception {
         TCPSocket tcpSocket = new TCPSocketImpl("127.0.0.1", 12324);
-        //tcpSocket.connect();
+        tcpSocket.connect();
         tcpSocket.send("1MB.txt");
         tcpSocket.close();
     }
