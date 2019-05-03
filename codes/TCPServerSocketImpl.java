616ad64fc69937d00791fde3787f4c76aa53ac0c
Mohammad Ali Toufighi <alitoumob@gmail.com>
2019-05-03 14:31:40 +0430

    improve accept

--- codes/TCPServerSocketImpl.java
+++ codes/TCPServerSocketImpl.java
@@ -1,8 +1,6 @@
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.SocketTimeoutException;
-import java.util.Arrays;
-import java.util.Objects;
 
 public class TCPServerSocketImpl extends TCPServerSocket {
     private static final int MAX_PAYLOAD_SIZE = EnhancedDatagramSocket.DEFAULT_PAYLOAD_LIMIT_IN_BYTES;
@@ -28,14 +26,17 @@ public class TCPServerSocketImpl extends TCPServerSocket {
     @Override
     public TCPSocket accept() throws Exception {
         state = TCPState.LISTEN;
-
+        int my_seq_number = Utils.randomInRange(50, 200); //havijoori :\
+        int my_ack_number = 0;
         udp_socket = new EnhancedDatagramSocket(port);
         udp_socket.setSoTimeout(5000); //TODO: ?
 
         boolean syn_received = false;
         while(!syn_received) {
-            try{
+            try {
                 TCPPacket recv_pkt = receivePacket();
+
+                my_ack_number = recv_pkt.getSeqNum() + 1;
                 if(recv_pkt.isSYN())
                     syn_received = true;
             } catch (SocketTimeoutException e) {
@@ -46,13 +47,18 @@ public class TCPServerSocketImpl extends TCPServerSocket {
 
         // send SYN-ACK and expect ACK
         boolean ack_received = false;
+
+        TCPPacket packet = new TCPPacket(true, true);
+        packet.setSeqNumber(my_seq_number++);
+        packet.setAckNumber(my_ack_number);
         while(!ack_received) {
-            TCPPacket packet = new TCPPacket(true, true);
-            byte[] buf = packet.toStream();
-            DatagramPacket send_dp = new DatagramPacket(buf, buf.length, address, peer_port);
-            udp_socket.send(send_dp);
+            sendPacket(packet);
             try {
                 TCPPacket recv_pkt = receivePacket();
+                if(recv_pkt.getAckNumber() != my_seq_number){
+                    System.out.println("BAD PACKET IN SV :|");
+                    //TODO: what now?
+                }
                 if(recv_pkt.isACK())
                     ack_received = true;
             } catch (SocketTimeoutException e) {
@@ -65,7 +71,14 @@ public class TCPServerSocketImpl extends TCPServerSocket {
         System.out.println("Server Established.");
         TCPSocketImpl ret = new TCPSocketImpl("127.0.0.1", peer_port);
         ret.setUdpSocket(udp_socket); //TODO: is correct?
+        ret.setSeqNum(my_seq_number);
         return ret;
+    }
+
+    private void sendPacket(TCPPacket packet) throws Exception {
+        byte[] buf = packet.toStream();
+        DatagramPacket dp = new DatagramPacket(buf, buf.length, address, peer_port);
+        udp_socket.send(dp);
     }
 
     @Override
